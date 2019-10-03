bf7b006ab2a760a00949d68d346e521f1b11360c
Joshua "Mart" Miller <32748201+Merchant151@users.noreply.github.com>
2019-10-03 08:06:47 -0400

    Puzzle up to part 2

---
+++ 15Puzzle/Assets/Move.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public Vector3 dest = new Vector3(1, 0, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dest = transform.position;
+        slideIfYouCan();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 delta = dest - transform.position;
+        Vector3 step = delta.normalized * Time.deltaTime;
+
+        if (step.magnitude < delta.magnitude)
+        {
+            transform.position += step;
+        }
+        else
+        {
+            transform.position = dest;
+        }
+    }
+    public void slideIfYouCan()
+    {
+        dest = transform.position + new Vector3(1, 0, 0);
+    }
+}
---
+++ 15Puzzle/Assets/PuzzleManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleManager : MonoBehaviour
+{
+    public float shuffleTime = 2;
+
+    Vector3[] startPositions;
+
+    private void Start()
+    {
+        startPositions = new Vector3[transform.childCount];
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            startPositions[i] = transform.GetChild(i).position;
+        }
+    }
+
+    void Update()
+    {
+        if (isShuffling())
+        {
+            //Random shuffles
+            Transform tile = transform.GetChild(Random.Range(0, transform.childCount));
+            tile.GetComponent<Move>().slideIfYouCan();
+        }
+        else
+        {
+            //Win check
+            bool isWin = true;
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                if (startPositions[i] != transform.GetChild(i).position)
+                    isWin = false;
+            }
+            if (isWin)
+            {
+                for (int i = 0; i < transform.childCount; i++)
+                {
+                    transform.GetChild(i).localScale = new Vector3(1, 1, 1);
+                }
+                Time.timeScale = 0; //stop time (pause game)
+            }
+        }
+    }
+
+    public bool isShuffling()
+    {
+        if (Time.time < shuffleTime)
+            return true;
+        else
+            return false;
+    }
+
+    public bool isEmptySpot(Vector3 pos)
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            if (transform.GetChild(i).GetComponent<Move>().dest == pos)
+                return false;
+        }
+        return true;
+    }
+}
