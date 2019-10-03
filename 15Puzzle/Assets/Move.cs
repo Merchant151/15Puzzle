44b65e8380a7a21cdf4e085958b76a3fa617d7e9
Joshua "Mart" Miller <32748201+Merchant151@users.noreply.github.com>
2019-10-03 08:40:05 -0400

    move script completed

--- 15Puzzle/Assets/Move.cs
+++ 15Puzzle/Assets/Move.cs
@@ -6,11 +6,14 @@ public class Move : MonoBehaviour
 {
     public Vector3 dest = new Vector3(1, 0, 0);
 
+    PuzzleManager puzzle;
+
     // Start is called before the first frame update
     void Start()
     {
         dest = transform.position;
-        slideIfYouCan();
+        puzzle = GameObject.Find("Grid").GetComponent<PuzzleManager>();
+        //slideIfYouCan();
     }
 
     // Update is called once per frame
@@ -28,8 +31,28 @@ public class Move : MonoBehaviour
             transform.position = dest;
         }
     }
+    private void OnMouseDown()
+    {
+        if(!puzzle.isShuffling())
+        slideIfYouCan();
+    }
     public void slideIfYouCan()
     {
-        dest = transform.position + new Vector3(1, 0, 0);
+        if (transform.position != dest)
+            return;
+        Vector3 rightDest = transform.position + new Vector3(1, 0, 0);
+        Vector3 leftDest = transform.position + new Vector3(-1, 0, 0);
+        Vector3 upDest = transform.position + new Vector3(0, 1, 0);
+        Vector3 downDest = transform.position + new Vector3(0, -1, 0);
+
+        if (puzzle.isEmptySpot(rightDest) && transform.position.x <= 2)
+            dest = rightDest;
+        else if (puzzle.isEmptySpot(leftDest) && transform.position.x >= 1)
+            dest = leftDest;
+        else if (puzzle.isEmptySpot(upDest) && transform.position.y <= 2)
+            dest = upDest;
+        else if (puzzle.isEmptySpot(downDest) && transform.position.y >= 1)
+            dest = downDest;
+
     }
 }
